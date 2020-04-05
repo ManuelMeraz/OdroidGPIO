d1904913149f9c2e1c20241e7d34acfb7a08aeb6
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-05 18:24:35 -0400

    Change include interface to include/OdroidGPIO remove accidently linked tests from odroidgpio library

--- tests/test_pwm_pin.cpp
+++ tests/test_pwm_pin.cpp
@@ -3,14 +3,14 @@
 
 auto main() -> int
 {
-   if (!gpio::is_ready()) {
-      return 1;
-   }
-
-   namespace pwm = gpio::pwm;
-   auto& pwm_pin = gpio::get<pwm::PWMPin>(10, pwm::Mode::OUTPUT);
-
-   std::cout << "mode: " << static_cast<int>(pwm_pin.mode()) << std::endl;
-   std::cout << "pin_number: " << static_cast<int>(pwm_pin.pin_number()) << std::endl;
-   return 0;
+   //   if (!gpio::is_ready()) {
+   //      return 1;
+   //   }
+   //
+   //   namespace pwm = gpio::pwm;
+   //   auto& pwm_pin = gpio::get<pwm::PWMPin>(10, pwm::Mode::OUTPUT);
+   //
+   //   std::cout << "mode: " << static_cast<int>(pwm_pin.mode()) << std::endl;
+   //   std::cout << "pin_number: " << static_cast<int>(pwm_pin.pin_number()) << std::endl;
+   //   return 0;
 }
