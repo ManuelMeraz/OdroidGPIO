fe43410441d39f9c74cc47fa58d4303e408d9d30
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-06 12:57:03 -0400

    made sure second isntance works

--- tests/test_digital_pin.cpp
+++ tests/test_digital_pin.cpp
@@ -13,5 +13,14 @@ auto main() -> int
 
    std::cout << "mode: " << static_cast<int>(digital_pin.mode()) << std::endl;
    std::cout << "pin_number: " << static_cast<int>(digital_pin.pin_number()) << std::endl;
+
+   digital_pin.mode(digital::Mode::INPUT);
+
+   auto& second_pin_instance = gpio::get<digital::DigitalPin>(10);
+   std::cout << "second instance mode: " << static_cast<int>(second_pin_instance.mode())
+             << std::endl;
+
+   std::cout << "first instance mode should be 0 -> " << static_cast<int>(digital_pin.mode())
+             << std::endl;
    return 0;
 }
