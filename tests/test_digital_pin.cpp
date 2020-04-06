b11c13af4c6adef465372d4a39fb079e3578bad2
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-06 14:53:31 -0400

    add in test to catch incorrect type

--- tests/test_digital_pin.cpp
+++ tests/test_digital_pin.cpp
@@ -11,16 +11,26 @@ auto main() -> int
    auto& digital_pin = gpio::get<digital::DigitalPin>(10, digital::Mode::OUTPUT);
    digital_pin.write(digital::IO::LOW);
 
-   std::cout << "mode: " << static_cast<int>(digital_pin.mode()) << std::endl;
-   std::cout << "pin_number: " << static_cast<int>(digital_pin.pin_number()) << std::endl;
+   std::cout << "mode: " << static_cast<uint16_t>(digital_pin.mode()) << std::endl;
+   std::cout << "pin_number: " << digital_pin.pin_number() << std::endl;
 
    digital_pin.mode(digital::Mode::INPUT);
 
    auto& second_pin_instance = gpio::get<digital::DigitalPin>(10);
-   std::cout << "second instance mode: " << static_cast<int>(second_pin_instance.mode())
+   std::cout << "second instance mode: " << static_cast<uint16_t>(second_pin_instance.mode())
              << std::endl;
 
-   std::cout << "first instance mode should be 0 -> " << static_cast<int>(digital_pin.mode())
+   std::cout << "first instance mode should be 0 -> " << static_cast<uint16_t>(digital_pin.mode())
              << std::endl;
+
+   namespace pwm = gpio::pwm;
+   try {
+      gpio::get<pwm::PWMPin>(10, pwm::Mode::OUTPUT);
+      std::cerr << "Failed to catch incorrect pin type." << std::endl;
+      return 1;
+   } catch (const std::invalid_argument& e) {
+      std::cout << "Caught incorrect pin type. Passed." << std::endl;
+   }
+
    return 0;
 }
