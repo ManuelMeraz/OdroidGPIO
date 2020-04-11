d795a040dcdf18d53e9e59356a5cf730cecb2ef4
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-11 16:55:52 -0400

    don't throw if duty cycle greater than 100. Instead give warning and set to 100.

--- src/pwm/PWMPin.cpp
+++ src/pwm/PWMPin.cpp
@@ -1,5 +1,6 @@
 #include <detail/gpio.hpp>
 #include <detail/pwm/pwm.hpp>
+#include <iostream>
 #include <odroid/pwm/PWMPin.hpp>
 
 gpio::pwm::PWMPin::PWMPin(uint16_t pin_number, Mode mode) : BasePin(pin_number), m_mode(mode)
@@ -33,7 +34,8 @@ auto gpio::pwm::PWMPin::duty_cycle() const -> uint16_t
 void gpio::pwm::PWMPin::duty_cycle(uint16_t duty_cycle)
 {
    if (duty_cycle > 100) {
-      throw std::invalid_argument("Duty cycle must be: [0, 100]");
+      std::cerr << "[WARNING] Duty cycle greater than 100. Setting to 100." << std::endl;
+      duty_cycle = 100;
    }
 
    m_duty_cycle = duty_cycle;
