63285ab5b191c07ac6f8d4ab3c2e6817ab3169e2
Manuel Meraz <meraz.manuel@gmail.com>
2020-04-15 08:36:03 -0400

    remove dead code in pwm example

--- examples/pwm_duty_cycle.cpp
+++ examples/pwm_duty_cycle.cpp
@@ -10,10 +10,9 @@ auto main() -> int
    }
 
    auto& pwm_pin = gpio::get<pwm::PWMPin>(PIN_NUMBER, pwm::Mode::OUTPUT);
-   // set clock or range here optionally
-   // pwm::clock(128) in hz
-   // pwm::range(500) some range here
-   pwm_pin.duty_cycle(50); // Some duty cycle [0, 100]
+
+   constexpr uint8_t DUTY_CYCLE_PERCENT = 50;
+   pwm_pin.duty_cycle(DUTY_CYCLE_PERCENT);
 
    return 0;
 }
